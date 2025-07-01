5bedf296365b24d24f4c5ff414bac2fd3b0b583c
carolinmimmie <carolinmimmie@gmail.com>
2025-07-01 12:07:18 +0200

    Initail commit

---
+++ components/Home/Navbar/Nav.tsx
@@ -0,0 +1,73 @@
+"use client";
+import ThemeToggler from "@/components/Helper/ThemeToggler";
+import { NavLinks } from "@/constant/constant";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { HiBars3BottomRight } from "react-icons/hi2";
+import { LuDumbbell } from "react-icons/lu";
+
+type Props = {
+  openNav: () => void;
+};
+
+export const Nav = ({ openNav }: Props) => {
+  const [navBg, setNavBg] = useState(false);
+
+  useEffect(() => {
+    const handler = () => {
+      if (window.scrollY >= 90) setNavBg(true);
+      if (window.scrollY < 90) setNavBg(false);
+    };
+    window.addEventListener("scroll", handler);
+    return () => window.removeEventListener("scroll", handler);
+  }, []);
+
+  return (
+    <div
+      className={`transition-all ${
+        navBg ? "bg-white dark:bg-gray-900 shadow-md" : "fixed"
+      } duration-200 h-[12vh] z-[10000] fixed w-full`}
+    >
+      <div className="flex items-center h-full justify-between w-[92%] mx-auto">
+        <div className="flex items-center sm:space-x-20">
+          <div className="flex items-center space-x-20">
+            <div className="w-10 h-10 bg-cyan-800 dark:bg-white rounded-full flex items-center justify-center flex-col">
+              <LuDumbbell className="w-5 h-5 text-white dark:text-black" />
+            </div>
+            <h1 className="text-xl hidden sm:block md:text-2xl text-cyan-800  dark:text-white font-bold">
+              CoreX
+            </h1>
+          </div>
+
+          <div className="hidden lg:flex items-center space-x-10">
+            {NavLinks.map((link) => {
+              return (
+                <Link
+                  key={link.id}
+                  href={link.url}
+                  className="text-base hover:text-cyan-700 dark:hover:text-cyan-200 font-medium transition-all duration-200"
+                >
+                  <p>{link.label}</p>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+        <div className="flex items-center space-x-4">
+          {" "}
+          <button className="px-8 py-2.5 text-xs sm:text-sm rounded-lg cursor-pointer bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-900 hover:bg-gray-300 transition-all duraction-300">
+            Login/register
+          </button>
+          <button className="px-8 py-2.5 text-sm hidden sm:block cursor-pointer rounded-lg bg-cyan-700 hover:bg-cyan-900 transition-all duration-300 text-white">
+            Log Workout
+          </button>
+          <ThemeToggler />
+          <HiBars3BottomRight
+            onClick={openNav}
+            className="w-8 h-8 cursor-pointer text-black lg:hidden"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ constant/constant.ts
@@ -0,0 +1,8 @@
+export const NavLinks = [
+  { id: 1, url: "#", label: "Home" },
+  { id: 2, url: "#", label: "Find Gym" },
+  { id: 3, url: "#", label: "Workouts" },
+  { id: 4, url: "#", label: "Nutrition" },
+  { id: 5, url: "#", label: "Blog" },
+  { id: 6, url: "#", label: "Contact" },
+];
